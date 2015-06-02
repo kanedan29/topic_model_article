9c0cfdd941246b576a97c208313948e1251564b5
Paul Roge <proge@riseup.net>
2015-06-02 10:06:32 -0400

    I updated the intersections between databases, but this creates an impossible scaled version. I will opt to an unscaled plot using VennDiagram, once again.

--- venn-databases.R
+++ venn-databases.R
@@ -48,6 +48,26 @@ n24 <- length(n24)
 n34 <- d[grep("(?=.*WOS)(?=.*Scopus)", d, perl=T)]
 n34 <- length(n34)
 
+## n123 counts
+
+n123 <- d[grep("(?=.*AGRICOLA)(?=.*ScienceDirect)(?=.*WOS)", d, perl=T)]
+n123 <- length(n123)
+
+## n124 counts
+
+n124 <- d[grep("(?=.*AGRICOLA)(?=.*ScienceDirect)(?=.*Scopus)", d, perl=T)]
+n124 <- length(n124)
+
+## n134 counts
+
+n134 <- d[grep("(?=.*AGRICOLA)(?=.*WOS)(?=.*Scopus)", d, perl=T)]
+n134 <- length(n134)
+
+## n234 counts
+
+n234 <- d[grep("(?=.*ScienceDirect)(?=.*WOS)(?=.*Scopus)", d, perl=T)]
+n234 <- length(n234)
+
 ## n1234 counts
 
 n1234 <- d[grep("(?=.*ScienceDirect)(?=.*AGRICOLA)(?=.*WOS)(?=.*Scopus)", d, perl=T)]
@@ -55,7 +75,7 @@ n1234 <- length(n1234)
 
 ## Create the Graph
 v <- venneuler(c(A=areas[1], B=areas[2], C=areas[3], D=areas[4],
-                 `A&B`=n12,`A&C`=n13,`A&D`=n14,`B&C`=n23,`B&D`=n24,`C&D`=n34,`A&B&C&D`=n1234)
+                 `A&B`=n12,`A&C`=n13,`A&D`=n14,`B&C`=n23,`B&D`=n24,`C&D`=n34,`A&B&C`=n123,`A&B&D`=n124,`A&C&D`=n134,`B&C&D`=n234,`A&B&C&D`=n1234)
                )
 v$labels <- c("AGRICOLA","ScienceDirect","Web of Science","Scopus")
 
