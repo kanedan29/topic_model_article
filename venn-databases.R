0e49ee3d4e0ca0bc24c7f43bc5d38871f93f1003
Paul Roge <proge@riseup.net>
2015-06-02 09:41:18 -0400

    Updated to reflect 4 databases (including Scopus). Also removed script to clean up entries without tags and tags related to cassava.

--- venn-databases.R
+++ venn-databases.R
@@ -4,18 +4,13 @@ require("venneuler")
 
 ## Import Data
 
-d <- as.character(read.csv("Zotero_db_dups_removed.csv")$Manual.Tags)
-
-## Remove empty tags, and cassava articles
-
-d <- d[grep("AGRICOLA|ScienceDirect|WOS", d)]
-d <- d[grep("cassava|manihot", d, invert=T)]
+d <- as.character(read.csv("P_grain_biblio_paper_database.csv")$Manual.Tags)
 
 ## Area counts
 
 areas <- c()
 
-for (i in c("AGRICOLA","ScienceDirect","WOS")){
+for (i in c("AGRICOLA","ScienceDirect","WOS","Scopus")){
     areas <- c(areas,
                length(
                    d[grep(i, d)]
@@ -33,21 +28,36 @@ n12 <- length(n12)
 n13 <- d[grep("(?=.*AGRICOLA)(?=.*WOS)", d, perl=T)]
 n13 <- length(n13)
 
+## n14 counts
+
+n14 <- d[grep("(?=.*AGRICOLA)(?=.*Scopus)", d, perl=T)]
+n14 <- length(n14)
+
 ## n23 counts
 
 n23 <- d[grep("(?=.*ScienceDirect)(?=.*WOS)", d, perl=T)]
 n23 <- length(n23)
 
-## n123 counts
+## n24 counts
 
-n123 <- d[grep("(?=.*ScienceDirect)(?=.*AGRICOLA)(?=.*WOS)", d, perl=T)]
-n123 <- length(n123)
+n24 <- d[grep("(?=.*ScienceDirect)(?=.*Scopus)", d, perl=T)]
+n24 <- length(n24)
+
+## n34 counts
+
+n34 <- d[grep("(?=.*WOS)(?=.*Scopus)", d, perl=T)]
+n34 <- length(n34)
+
+## n1234 counts
+
+n1234 <- d[grep("(?=.*ScienceDirect)(?=.*AGRICOLA)(?=.*WOS)(?=.*Scopus)", d, perl=T)]
+n1234 <- length(n1234)
 
 ## Create the Graph
-v <- venneuler(c(A=areas[1], B=areas[2], C=areas[3],
-                 `A&B`=n12,`A&C`=n13,`B&C`=n23,`A&B&C`=n123)
-               )              
-v$labels <- c("AGRICOLA","ScienceDirect","Web of Science")
+v <- venneuler(c(A=areas[1], B=areas[2], C=areas[3], D=areas[4],
+                 `A&B`=n12,`A&C`=n13,`A&D`=n14,`B&C`=n23,`B&D`=n24,`C&D`=n34,`A&B&C&D`=n1234)
+               )
+v$labels <- c("AGRICOLA","ScienceDirect","Web of Science","Scopus")
 
 ## Create the Plot
 
