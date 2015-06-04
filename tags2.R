3add98c32d4720a693f58ce18586db2784af1d31
kanedan29 <kanedan29@gmail.com>
2015-06-04 11:52:44 -0400

    Made new tags script to match new tags in db.

---
+++ tags2.R
@@ -0,0 +1,6 @@
+### generate tags for subsetting data ####
+
+all.tags <- list("search.grain","search.pigeonpea","search.rice","search.rye","search.sorghum","search.wheat")
+
+crop.names <- c("grain","pp","rice","rye","sorghum","wheat")
+
