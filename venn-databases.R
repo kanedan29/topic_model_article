761eefcffdc77e1781c5ee1a55d27e26b08c35f5
Paul Roge <proge@riseup.net>
2015-06-04 11:59:55 -0400

    The venn diagram is now divided into two views: DATABASE and CROP

--- venn-databases.R
+++ venn-databases.R
@@ -13,17 +13,25 @@ d <- as.character(read.csv("P_grain_biblio_paper_database.csv")$Manual.Tags)
 cat.names <- c()
 for (i in c("AGRICOLA","ScienceDirect","Scopus","Web of Science")){
     cat.names <- cbind(cat.names,
-                           paste(i,"\n","(",length(d[grep(i, d)]),")",sep=""))
+                       paste(i,
+                             "\n", "(", length(d[grep(i, d)]), ")", sep=""))
+}
+
+for (i in c("search.pigeonpea", "search.rice", "search.rye",
+            "search.sorghum", "search.wheat")){
+    cat.names <- cbind(cat.names,
+                       paste(unlist(strsplit(i,"[.]"))[2],
+                             "\n", "(", length(d[grep(i, d)]),")",sep=""))
 }
 
 ## Create Color Palette
 
-fill.colors <- brewer.pal(4, "Accent")
+fill.colors <- brewer.pal(9, "Set3")
 
 
 ## Create the Graphs
 
-v.quint <- venn.diagram(
+v.db <- venn.diagram(
     list(A= grep("AGRICOLA",d),
          B= grep("ScienceDirect",d),
          C= grep("Scopus",d),
@@ -32,14 +40,44 @@ v.quint <- venn.diagram(
     filename=NULL,
     fontfamily = "sans",
     cat.fontfamily = "sans",
-    category.names = cat.names,
-    fill = fill.colors,
+    sub.fontfamily = "sans",
+    cat.dist = c(.25, .25, .15, .15),
+    sub.pos = c(0.09, 1.05),
+    category.names = cat.names[1:4],
+    sub = "(a) DATABASES",
+    fill = fill.colors[1:4],
+    alpha = .4,
+    col = "darkgray"
+    )
+
+v.cp <- venn.diagram(
+    list(A= grep("search.pigeonpea",d),
+         B= grep("search.rice",d),
+         C= grep("search.rye",d),
+         D= grep("search.sorghum",d),
+         E= grep("search.wheat",d)
+         ),
+    filename=NULL,
+    fontfamily = "sans",
+    cat.fontfamily = "sans",
+    sub.fontfamily = "sans",
+    cat.dist = .25,
+    sub.pos = c(0, 1.05),
+    sub = "(b) CROPS",
+    category.names = cat.names[5:9],
+    fill = fill.colors[5:9],
     alpha = .4,
     col = "darkgray"
     )
 
 ## Create the Plot
 
-pdf("venn-databases.pdf")
-grid.draw(v.quint)
+pdf("venn-databases.pdf", width=4.5,height=9)
+pushViewport(viewport(layout = grid.layout(2, 1), height=.85, width=.85))
+pushViewport(viewport(layout.pos.col = 1, layout.pos.row = 1))
+grid.draw(v.db)
+popViewport()
+pushViewport(viewport(layout.pos.col = 1, layout.pos.row = 2))
+grid.draw(v.cp)
+popViewport()
 dev.off()
