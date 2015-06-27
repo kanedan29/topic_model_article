3fc2132fc6475ed90a6721cb1dd0d4ff7a15179d
Paul Roge <proge@riseup.net>
2015-06-27 00:20:31 -0400

    Working version to produce graphs with counts on y axis. Will now attempt to swap with counts on x axis.

--- graphs-publications.R
+++ graphs-publications.R
@@ -1,29 +1,21 @@
-## Packages
-## install.packages("RColorBrewer")
-## install.packages("ggplot2")
-require(RColorBrewer)
-require(ggplot2)
-require(reshape2)
-require(plyr)
-
 ## Import Data
 d <- data.frame(year = read.csv("P_grain_biblio_paper_database.csv")$Publication.Year,
-                publication = as.character(
+                Publication = as.character(
                     read.csv("P_grain_biblio_paper_database.csv")$Publication.Title),
                 tags = read.csv("P_grain_biblio_paper_database.csv")$Manual.Tags
                 )
 
 ## Replace "&" with "and"
-d$publication <- gsub("&", "and", d$publication, ignore.case=T)
+d$Publication <- gsub("&", "and", d$Publication, ignore.case=T)
 
 ## Remove punctuation
-d$publication <- gsub("[[:punct:]]", "", d$publication)
+d$Publication <- gsub("[[:punct:]]", "", d$Publication)
 
 ## Remove extra spaces
-d$publication <- gsub("[ ]+", " ", d$publication, perl=T)
+d$Publication <- gsub("[ ]+", " ", d$Publication, perl=T)
 
 ## upcase
-d$publication <- toupper(d$publication)
+d$Publication <- toupper(d$Publication)
 
 ## journal titles per period
 
@@ -33,46 +25,67 @@ d$period[d$year >= 1991 & d$year <= 2015] <- "1991-2015"
 
 ## list per crop
 
-d.list <- list(Grain = d[grep("search.grain", d$tags),][,c(2,4)],
-               Pigeonpea = d[grep("search.pigeonpea", d$tags),][,c(2,4)],
-               Rice = d[grep("search.rice", d$tags),][,c(2,4)],
-               Sorghum = d[grep("search.sorghum", d$tags),][,c(2,4)],
-               Wheat = d[grep("search.wheat", d$tags),][,c(2,4)],
-               All = d[,c(2,4)])
+d.list <- list(grain = d[grep("search.grain", d$tags),][,c(2,4)],
+               pigeonpea = d[grep("search.pigeonpea", d$tags),][,c(2,4)],
+               rice = d[grep("search.rice", d$tags),][,c(2,4)],
+               sorghum = d[grep("search.sorghum", d$tags),][,c(2,4)],
+               wheat = d[grep("search.wheat", d$tags),][,c(2,4)],
+               all = d[,c(2,4)])
 
-## Summarize data: (1) count publications per period, (2) calculate
-## the total publication count for the period, (3) normalize counts
+## Count publications per period,
 
-d.list.summary <- lapply(d.list, ddply, .(period, publication),
+require(reshape2)
+require(plyr)
+
+d.list.summary <- lapply(d.list, ddply, .(period, Publication),
                          summarise,
-                         n.pub = length(publication))
+                         Count = length(Publication))
+
+## Calculate publication count for the period, and normalize counts
 
 d.list.summary <- lapply(d.list.summary, ddply, .(period),
                          mutate,
-                         n.period = length(publication),
-                         n.norm = n.pub/n.period)
+                         n.period = length(Publication),
+                         n.norm = Count/n.period)
 
-lapply(d.list.summary, transform, order(-d.list$i$n.norm,d.list$i$publication),])
-     }
+## Flatten, arrange dataframe, and remove incomplete entries
 
+d.df.summary <- ldply(d.list.summary)
+d.df.summary <- arrange(d.df.summary, .id, desc(period), desc(n.norm),
+                        Publication)
+d.df.summary <- d.df.summary[complete.cases(d.df.summary),]
 
-for (i in names(d.list.summary)){
-transform(d.list.summary$i, d.list.summary$All[with(d.list.summary$All, order(period,-n.norm, publication)),])
-}
+## Select top 10 publications per time period
 
+d.df.summary <- ddply(d.df.summary, .(.id, period), function(x) x[1:10,])
 
-publication = strwrap(publication, width=.3)
+detach(package:reshape2, unload=T)
+detach(package:plyr, unload=T)
+
+## Wrap names and select complete cases again (not sure more NAs are generated)
+
+require(stringr)
+d.df.summary$Publication <- str_wrap(d.df.summary$Publication, width=30)
+detach(package:stringr, unload=T)
+
+d.df.summary <- d.df.summary[complete.cases(d.df.summary),]
 
 ## Create Plots
 
-for (i in names(d.list){
-         g <- ggplot(data=d.list$i, aes(y=freq,fill=Publication.Title))
-g + geom_bar(aes(x=Publication.Title),stat="identity")+
-  ylab("Count")+
-  xlab("")+
-  theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+
-  scale_fill_manual(values=alpha(fill.colors), name="Journal")+
-  scale_y_continuous(breaks=pretty_breaks())
-dx.journal.hist
+require(ggplot2)
+require(grid)
 
+for (i in unique(d.df.summary$.id)){
 
+    g <- ggplot(d.df.summary[d.df.summary$.id == i,],
+                aes(x=Publication, y= Count))+
+        geom_bar(stat="identity")+
+            facet_grid(. ~ period, scale = "free_x")+
+                theme(axis.text.x = element_text(angle = 45,vjust= 1,hjust=1),
+                      plot.margin = unit(c(0.5,0.5,0.5,2), "cm"))
+    
+    ggsave(g,file=paste(getwd(),"/figures/","publications-",i,".pdf",sep=""))
+}
+
+detach(package:ggplot2, unload=T)
+detach(package:grid, unload=T)
