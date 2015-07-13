7cb3eb8c3afef500847a9404fc13a5988cb682a2
Paul Roge <proge@riseup.net>
2015-07-13 17:03:52 -0400

    Added script for cleaning up publication names. Removed shading from publication and topic graphs, and added info on them to the x-axis. Renamed output files. Removed exraneous code and edited comments.

--- graphs.R
+++ graphs.R
@@ -6,9 +6,9 @@ library(grid)
 library(fields)
 library(RColorBrewer)
 library(scales)
+ 
+## Topic histogram per decade
 
-  
-### topic assignment histograms over time #####
 years <- c("1920-1930","1930-1940","1940-1950","1950-1960","1960-1970","1970-1980","1980-1990",
                      "1990-2000","2000-2010","2010-2015")
 
@@ -24,8 +24,7 @@ names(dx.topic.time) <- c("Year","Topic","Count")
 dx.topic.time$Topic <- as.factor(dx.topic.time$Topic)
 dx.topic.time$Count <- as.numeric(dx.topic.time$Count)
 
-
-topic.year <- ggplot(data=dx.topic.time, aes(x=Year,y=Count,group=Topic, fill=Topic))+
+topic.decade <- ggplot(data=dx.topic.time, aes(x=Year,y=Count,group=Topic, fill=Topic))+
   geom_bar(stat="identity", position="dodge")+
   scale_fill_grey(start=0.3,end=0.7)+
   ylab("Count")+
@@ -34,57 +33,53 @@ topic.year <- ggplot(data=dx.topic.time, aes(x=Year,y=Count,group=Topic, fill=To
   guides(fill=guide_legend(title="Selected\nTopic Terms"))+
   scale_y_continuous(breaks=pretty_breaks())
 
-topic.year
-
-##### METADATA GRAPHS #######
+## Overall topic counts
 
 dx.topic.count <- count(dx, vars=c("topic.assign"))
 dx.topic.count$freq <- as.numeric(dx.topic.count$freq)
+for (i in 1:3){
+    dx.topic.count$labels[dx.topic.count$topic.assign == i] <- labels[i]
+}
 
-dx.topic.hist <-ggplot(data=dx.topic.count,aes(y=freq))+
-  geom_bar(aes(x=topic.assign),fill="grey", color="black", stat="identity")+
-  ylab("Count")+
-  xlab("Topic")+
-  scale_y_continuous(breaks=pretty_breaks())
-dx.topic.hist
+dx.topic.hist <-ggplot(data=dx.topic.count,aes(y=freq, x=labels))+
+    geom_bar(stat="identity")+
+      ylab("Count")+
+          xlab("Topic and Associated Terms")+
+              scale_y_continuous(breaks=pretty_breaks())
 
-fill.colors <- brewer.pal(9, "Greens")[5:9]
+## Overall publication counts
+
+### Replace "&" with "
+dx$Publication.Title <- gsub("&", "and", dx$Publication.Title, ignore.case=T)
+
+### Remove punctuation
+dx$Publication.Title <- gsub("[[:punct:]]", "", dx$Publication.Title)
+
+### Remove extra spaces
+dx$Publication.Title <- gsub("[ ]+", " ", dx$Publication.Title, perl=T)
+
+### Upcase
+dx$Publication.Title <- toupper(dx$Publication.Title)
+
+### Wrap names
+
+dx$Publication.Title <- str_wrap(dx$Publication.Title, width=40)
+
+### Publication counts
 dx.journal <- count(dx, vars="Publication.Title")
 dx.journal <- as.data.frame(dx.journal[order(-dx.journal$freq,dx.journal$Publication.Title),])
 dx.journal$Publication.Title <- factor(dx.journal$Publication.Title, levels=dx.journal$Publication.Title)
 
-dx.journal.hist <-ggplot(data=dx.journal[1:5,], aes(y=freq,fill=Publication.Title))+
-  geom_bar(aes(x=Publication.Title),stat="identity")+
+### Plot publications
+dx.journal.hist <-ggplot(data=dx.journal[dx.journal$freq >= 15,], aes(y=freq,x=Publication.Title))+
+  geom_bar(stat="identity")+
   ylab("Count")+
-  xlab("")+
-  theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+
-  scale_fill_manual(values=alpha(fill.colors), name="Journal")+
-  scale_y_continuous(breaks=pretty_breaks())
-dx.journal.hist
+      xlab("Publications with more than 15 articles")+
+          theme(axis.text.x=element_text(angle=45, hjust=1,vjust=1,size=10),
+                plot.margin = unit(c(.25,.25,.25,1.5), "in"))+
+              scale_y_continuous(breaks=pretty_breaks())
 
-
-ggsave(dx.topic.hist,file=paste(getwd(),"/figures/",dx.tag,"_topic_hist.pdf",sep=""))
-ggsave(dx.journal.hist,file=paste(getwd(),"/figures/",dx.tag,"_journal_hist.pdf",sep=""))
-ggsave(topic.year,file=paste(getwd(),"/figures/",dx.tag,"_topic_year.pdf",sep=""))
-
-
-
-#ggsave(dx.year.line,file=paste(getwd(),"/figures/",dx.tag,"_year_line.pdf",sep=""))
-#ggsave(dx.year.bar,file=paste(getwd(),"/figures/",dx.tag,"_year_bar.pdf",sep=""))
-
-
-#dx.year <- count(dx, vars=c("Publication.Year"))
-
-
-#dx.year.line <- ggplot(data=dx.year[dx.year$Publication.Year >= 1900,], aes(x=Publication.Year, y=freq))+
- # geom_line(color="black")+
-  #ylab("Count")+
-  #xlab("")
-#dx.year.line
-
-#dx.year.bar <- ggplot(data=dx.year[dx.year$Publication.Year >= 1900,], aes(x=Publication.Year, y=freq))+
- # geom_bar(stat="identity", fill="grey",color="black")+
-  #ylab("Count")+
-  #xlab("")
-#dx.year.bar
-
+ggsave(dx.topic.hist,file=paste(getwd(),"/figures/","topic-hist-",dx.tag,".pdf",sep=""))
+ggsave(dx.journal.hist,file=paste(getwd(),"/figures/","publication-hist-",dx.tag,".pdf",sep=""))
+ggsave(topic.decade,file=paste(getwd(),"/figures/","topic-hist-decade-",dx.tag,".pdf",sep=""))
+dev.off()
